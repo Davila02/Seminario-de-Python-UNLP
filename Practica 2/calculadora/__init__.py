1109ba17ae34a18d329102d3f872a07fe3d3c4a9
Jorge Davila <jedavila2002@gmail.com>
2024-04-12 18:28:54 -0300

    Resolucion del inciso 10.2

--- Practica 2/calculadora/__init__.py
+++ Practica 2/calculadora/__init__.py
@@ -7,3 +7,7 @@ def generar_estructura(names,goals,goals_avoided,assists):
 
     return players_stats
 
+def goleador(player_stats):
+    max_player, cant_goals = max(player_stats.items(), key=lambda elem: elem[1][0])
+    info = (max_player,cant_goals[0])
+    return info
