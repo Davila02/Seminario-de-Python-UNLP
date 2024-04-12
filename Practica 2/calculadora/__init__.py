09b4db0bb14d01933fa75fa83391588272f19bfa
Jorge Davila <jedavila2002@gmail.com>
2024-04-12 18:31:18 -0300

    TP 2: resolucion del inciso 10.3

--- Practica 2/calculadora/__init__.py
+++ Practica 2/calculadora/__init__.py
@@ -11,3 +11,13 @@ def goleador(player_stats):
     max_player, cant_goals = max(player_stats.items(), key=lambda elem: elem[1][0])
     info = (max_player,cant_goals[0])
     return info
+
+def calcular_promedio_ponderado(player_stats):
+    promedio_jugadores = []
+    for player, stats in player_stats.items():
+        promedio_ponderado = (player, (stats[0] * 1.5) + (stats[1] * 1.25) + stats[2])
+        promedio_jugadores.append(promedio_ponderado)
+    return promedio_jugadores
+
+def mas_influyente(player_stats):
+    return max(calcular_promedio_ponderado(player_stats), key=lambda elem:elem[1])[0]
